953ec302b429aeb90a4bc5e3bc3531fdd517aff8
Mattia Astorino <astorino.mattia@gmail.com>
2018-09-02 11:54:18 +0200

    chore: Update starting template

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,8 @@ module.exports = {
       extensions : '.pcss',
       plugins    : [require('stylelint')()]
     },
-    'postcss-preset-env': {
+    'postcss-responsive-type' : {},
+    'postcss-preset-env'      : {
       browsers     : '> 0.25%, not dead',
       stage        : 0,
       autoprefixer : {
