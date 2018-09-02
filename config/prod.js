d8c1248c60f6c66e4c8e7ad69400a329bc7b7888
king-prawns <giorgio.gamberoni@gmail.com>
2018-09-02 18:02:17 +0200

    update error handling

--- config/prod.js
+++ config/prod.js
@@ -13,13 +13,26 @@ fs.remove(path.resolve(__dirname, '../dist'), (err) => {
     spinner.fail('error - fs.remove');
     throw err;
   }
-  webpack(config, (werr, stats, e) => {
+  webpack(config, (werr, stats) => {
     console.log('\n');
 
-    if (werr) {
+    if (werr || stats.hasErrors()) {
+      console.log(stats.toString({
+        hash        : false,
+        timings     : false,
+        modules     : false,
+        chunks      : false,
+        colors      : true,
+        assets      : false,
+        children    : false,
+        entrypoints : false
+      }));
+
+      console.log('\n');
       spinner.fail('Build error. 😭');
-      throw werr;
+      return;
     }
+
     process.stdout.write(`${stats.toString({
       colors       : true,
       modules      : false,
@@ -29,7 +42,7 @@ fs.remove(path.resolve(__dirname, '../dist'), (err) => {
     })}\n\n`);
 
     spinner.succeed('Build complete. 🎉\n');
-    console.log(chalk.yellow(' Tip: built files are meant to be served over an HTTP server.\n' +
-      ' Opening index.html over file:// won\'t work.\n'));
+    console.log(chalk.yellow(' Tip: built files are meant to be served over an HTTP server.\n'
+      + ' Opening index.html over file:// won\'t work.\n'));
   });
 });
